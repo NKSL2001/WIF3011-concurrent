89ce59e2dc17009548f86eb61b8c0ff0fe6fe31e
NKSL2001 <s2116593@siswa.um.edu.my>
2024-05-09 17:34:26 +0800

    remove redundant print

--- src/Sequential.java
+++ src/Sequential.java
@@ -9,7 +9,6 @@ public class Sequential {
         try (BufferedReader  sc = new BufferedReader (new FileReader("test text.txt"))) {
             String line;
             while((line = sc.readLine()) != null){
-                // System.out.println(line);
                 splitted.addAll(
                     Arrays.asList(line.split("\\W+"))
                     .stream()
@@ -19,18 +18,16 @@ public class Sequential {
                 );
             }
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
+            System.err.println("File not found");
             e.printStackTrace();
         }
         
-        // System.out.println(splitted);
         HashMap<String, Integer> counts = new HashMap<>();
         for (String word : splitted) {
             counts.put(word, counts.getOrDefault(word, 0) + 1);
         }
 
-        System.out.println(counts.size());
-        System.out.println(counts);
+        // store count results
         this.results = counts;
     }
 }
