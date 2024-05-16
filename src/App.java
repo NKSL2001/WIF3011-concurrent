6e52207e11644ebb2d47c135fb6eba7578f2c329
derrick phang <derrickphb@gmail.com>
2024-05-17 01:15:20 +0800

    Phang 17 May 2024: -Add RunnableRead2.java and RunnableCount2.java. -They perform BOW via Runnable threads, this time with 1 producer thread and 2 consumer threads. -In terms of execution time: a. While run as full code it displays 5~10 milliseconds. b. While run as individual section it displays 25~35 milliseconds.
    Additional check is advised.

--- src/App.java
+++ src/App.java
@@ -1,26 +1,28 @@
-
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-// ArrayList, List
+// ArrayList, List, PriorityQueue, HashMap
 public class App {
 
     static volatile List<String> processed = new ArrayList<>();
     static AtomicBoolean isReading = new AtomicBoolean(true);
+    static AtomicBoolean isReading2 = new AtomicBoolean(true);
+
+    static volatile String lineRead;
+    static volatile Queue<String> listRead = new PriorityQueue<>();
+    static volatile List<String> processed2 = new ArrayList<>();
+    static volatile HashMap<String, Integer> counts = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
-        String filePath = "test text.txt";
+        App app = new App(); // for Runnable 2.0 synchronized monitor
+
+        String filePath = "C:/WIF3011-concurrent/test text.txt";
 
         // Sequential
         long starttime = System.currentTimeMillis();
         Sequential sequentialResult = new Sequential(filePath);
         long diff = System.currentTimeMillis() - starttime;
         System.out.printf("Sequential taken %d milliseconds.\n", diff);
-
-        // // Separate output
-        // System.out.println("\n".repeat(10));
-        // System.out.println("=".repeat(150));
-        // System.out.println("\n".repeat(10));
 
         // Concurrent #1: via Runnable
         RunnableRead readFile = new RunnableRead(processed, isReading, filePath);
@@ -45,5 +47,25 @@ public class App {
         long diff3 = System.currentTimeMillis() - starttime3;
         System.out.printf("Future taken %d milliseconds.\n", diff3);
 
+        // Concurrent via Runnable 2.0
+        // 1 producer thread 2 consumer threads
+        RunnableRead2 readFile2 = new RunnableRead2(listRead, filePath, lineRead, app, isReading2);
+        RunnableCount2 countWord = new RunnableCount2(app, isReading2, listRead, processed2, counts);
+        RunnableCount2 countWord2 = new RunnableCount2(app, isReading2, listRead, processed2, counts);
+
+        Thread readThread2 = new Thread(readFile2);
+        Thread countThread2 = new Thread(countWord);
+        Thread countThread3 = new Thread(countWord2);
+
+        long starttime4 = System.currentTimeMillis();
+        readThread2.start();
+        countThread2.start();
+        countThread3.start();
+        
+        readThread2.join();
+        countThread2.join();
+        countThread3.join();
+        long diff4 = System.currentTimeMillis() - starttime4;
+        System.out.printf("Runnable 2.0 taken %d milliseconds.\n", diff4);
     }
 }
